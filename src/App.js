803058111ea603f5895b78d9be28ccee3047f997
Jesus Rodriguez <jrod1841@gmail.com>
2023-02-05 19:20:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { getPlayersFromData, getRandomPlayers, capitalizeName } from './service/api'
+import Button from './components/Button';
+import Header from './components/Header';
+import Player from './components/Player'
 
-function App() {
+
+const App = () => {
+  const [players, setPlayers] = useState([]);
+  const [randomPlayers, setRandomPlayers] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+
+  useEffect(() => {
+    getPlayersFromData()
+      .then(data => {
+        setPlayers(data.filter(player => player.name && player.position && player.teamId));
+        setRandomPlayers(getRandomPlayers(players));
+      });
+  }, []);
+
+  const handleRefreshClick = () => {
+    setRandomPlayers(getRandomPlayers(players));
+  };
+
+  const handlePlayerClick = player => {
+    setSelectedPlayer(selectedPlayer === player ? null : player);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+    <div className='container'>
+      <header>
+        <Header name='NBA API' />
+        <Button color='green' text='Get Players!' refreshPlayers={handleRefreshClick} />
       </header>
+      {randomPlayers.map(player => {
+        if (!player.name) return null;
+        return (
+          <Player 
+          key={player.name}
+          color='grey'
+          name={capitalizeName(player.name)}
+          position={player.position}
+          team={player.teamId}
+          ppg={player.ppg}
+          apg={player.apg}
+          rpg={player.rpg}
+          blk={player.blkPerG}
+          stl={player.stlPerG}
+          selected={selectedPlayer === player}
+          onClick={() => handlePlayerClick(player)}
+        />
+        )
+      })}
     </div>
   );
-}
+};
 
 export default App;
